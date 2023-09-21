8fa5c6ccafbb040578a7d2c1cca2cd1bc782854e
qu-ngx <quang13012005@gmail.com>
2023-09-21 09:29:19 -0400

    first commit

---
+++ mnist-classifier.py
@@ -0,0 +1,14 @@
+import tensorflow as tf
+
+# Import Tensorflow Datasets
+import tensorflow_datasets as tfds
+tfds.disable_progress_bar()
+
+# Helper libraries
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+
+import logging
+logger = tf.get_logger()
+logger.setLevel(logging.ERROR)
